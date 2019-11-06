ec3843b03ce006168b6904fa6d5ed1d5d953cd0d
andreslargo <felipeflogxd>
2019-11-05 23:56:01 -0500

    User

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {HomeComponent } from './components/home/home.component';
 import {SolisalaComponent} from './components/solisala/solisala.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'solisala', component: SolisalaComponent},
+  {path: 'user', component: SolisalaComponent},
   {path: '*', redirectTo: ''}
 ];
 
