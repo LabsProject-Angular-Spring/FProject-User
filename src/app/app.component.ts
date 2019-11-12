8b01c4c48d1e1fead2b6010fa380b9e191385ed0
Erika Marcela Infante Montaño <erikainfantedoo@gmail.com>
2019-11-12 06:24:55 -0500

    usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProgWeb';
+  Usuarios=[
+    {
+      Id: '1',
+      Nombre: 'Javier Alarcón',
+      Tipo: 'Estudiante',
+      email: 'javier-alarconm@unilibre.edu.co'
+    },
+    {
+      Id: '2',
+      Nombre: 'Carmen Contreras',
+      Tipo: 'Auxiliar',
+      email: 'CClinx@gmail.com'
+    },
+    {
+      Id: '3',
+      Nombre: 'Mariana Reyes',
+      Tipo: 'Estudiante',
+      email: 'marianaj-reyesu@unilibre.edu.co'
+    },
+    {
+      Id: '4',
+      Nombre: 'Mauricio Alonso',
+      Tipo: 'Docente',
+      email: 'mauricio-alonsom@unilibre.edu.co'
+    }
+  ]
 }
