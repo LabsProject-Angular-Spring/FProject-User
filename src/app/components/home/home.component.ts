6bb76378a04a41afd2a5d1679120a21e56c20fee
Erika Marcela Infante Montaño <erikainfantedoo@gmail.com>
2019-11-12 21:54:14 -0500

    interfaces usuario

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  Usuarios=[
+    {
+      Id: '1',
+      Nombre: 'Javier Alarcón',
+      Tipo: 'Estudiante',
+      email: 'javier-alarconm@unilibre.edu.co'
+    },
+    {
+      Id: '2',
+      Nombre: 'Carmen Contreras',
+      Tipo: 'Auxiliar',
+      email: 'CClinx@gmail.com'
+    },
+    {
+      Id: '3',
+      Nombre: 'Mariana Reyes',
+      Tipo: 'Estudiante',
+      email: 'marianaj-reyesu@unilibre.edu.co'
+    },
+    {
+      Id: '4',
+      Nombre: 'Mauricio Alonso',
+      Tipo: 'Docente',
+      email: 'mauricio-alonsom@unilibre.edu.co'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
